85586d7fc15ef49fd5e8becc35e3d77e881637a7
HaiderAlboloushi180 <halbolou05@gmail.com>
2022-03-12 12:35:36 +0300

    Done with bonus

--- Day4/app/src/main/java/com/example/day4/MainActivity2.java
+++ Day4/app/src/main/java/com/example/day4/MainActivity2.java
@@ -2,8 +2,12 @@ package com.example.day4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import  android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -14,12 +18,23 @@ public class MainActivity2 extends AppCompatActivity {
 
         TextView nameText = findViewById(R.id.name);
         TextView ageText = findViewById(R.id.age);
+        Button google = findViewById(R.id.button);
         Bundle baggage = getIntent().getExtras();
         String name = baggage.getString("name");
         String age  = baggage.getString("age");
 
         nameText.setText(name);
         ageText.setText(age);
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gowagole = new Intent(Intent.ACTION_VIEW);
+                String url = "https://www.google.com/search?q=tea&client=opera-gx&hs=Gy1&sxsrf=APq-WBstjZhwCRkGKTg4PvmU5Embkg8chw:1647077475133&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjQk8reocD2AhWFSvEDHQQvA2AQ_AUoAXoECAEQAw";
+                gowagole.setData(Uri.parse(url));
+                startActivity(gowagole);
+            }
+        });
 
     }
 }
