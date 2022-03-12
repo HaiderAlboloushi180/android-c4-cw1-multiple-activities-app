89364324b6a790b7158b15843f69846ae87fe7c7
HaiderAlboloushi180 <halbolou05@gmail.com>
2022-03-12 12:08:28 +0300

    done

--- Day4/app/src/main/java/com/example/day4/MainActivity.java
+++ Day4/app/src/main/java/com/example/day4/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.day4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText nameText = findViewById(R.id.name);
+        EditText ageText = findViewById(R.id.age);
+        Button nextPage = findViewById(R.id.next);
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextBage = new Intent(MainActivity.this, MainActivity2.class);
+                String isim = nameText.getText().toString();
+                String umr = ageText.getText().toString();
+
+                nextBage.putExtra("name", isim);
+                nextBage.putExtra("age", umr);
+
+                if (isim == null || isim.isEmpty()) {
+                    System.out.println("Please Enter the missing data");
+                } else if (umr == null || umr.isEmpty()) {
+                    System.out.println("Please Enter the missing data");
+                } else {
+                    startActivity(nextBage);
+                }
+            }
+        });
+
     }
 }
--- Day4/app/src/main/java/com/example/day4/MainActivity2.java
+++ Day4/app/src/main/java/com/example/day4/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.day4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView nameText = findViewById(R.id.name);
+        TextView ageText = findViewById(R.id.age);
+        Bundle baggage = getIntent().getExtras();
+        String name = baggage.getString("name");
+        String age  = baggage.getString("age");
+
+        nameText.setText(name);
+        ageText.setText(age);
+
     }
 }
